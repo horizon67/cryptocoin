edc772a6b76b71b6de25dd1f9d56365eec659ba8
horizon67 <sir.yes.sir.maru@gmail.com>
2018-02-15 19:51:30 +0900

    unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,3 +53,27 @@ set :yarn_roles, :app
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+      invoke 'unicorn:restart'
+    end
+  end
+
+  after :publishing, :restart
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  #after :finishing, 'deploy:cleanup'
+end
