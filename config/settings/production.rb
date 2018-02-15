8c9c1f60a5cb15c05d923bc6109482a517623b82
horizon67 <sir.yes.sir.maru@gmail.com>
2018-02-16 01:02:59 +0900

    設定ファイル

--- config/settings/production.rb
+++ config/settings/production.rb
@@ -1,6 +1,18 @@
 class Settings
   extend Dry::Configurable
 
+  setting :notify_shooting_up do
+    setting :notify_limit, 0.35
+  end
+
+  setting :notify_shooting_down do
+    setting :notify_limit, -0.35
+  end
+
+  setting :notify_best_arbitrage_btc do
+    setting :notify_limit, 20000
+  end
+
   setting :site do
     setting :name, "CoinDevActivitiy"
     setting :page_title, "Cryptocurrency Development Activitiy"
