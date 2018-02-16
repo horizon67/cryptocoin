37ba5bb2b8fc888c59dbf3d366ca8c54f78332c9
horizon67 <sir.yes.sir.maru@gmail.com>
2018-02-16 17:38:40 +0900

    速度改善

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,7 +1,7 @@
 class CoinsController < ApplicationController
   def index
     expires_in 3.hour
-    @coins = Coin.joins(:repositories).where("repositories.language is not null")
+    @coins = Coin.includes(:repositories).where("repositories.language is not null").references(:repositories)
     render json: @coins, root: false, adapter: :json
   end
 end
