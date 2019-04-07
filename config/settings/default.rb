d1be53c601184ff645b57da5d56f102835a3916b
horizon67 <sir.yes.sir.maru@gmail.com>
2019-04-07 13:21:40 +0900

    fix keyword

--- config/settings/default.rb
+++ config/settings/default.rb
@@ -35,7 +35,7 @@ class Settings
     setting :name, "CoinDevActivitiy"
     setting :page_title, "Cryptocurrency Development Activitiy"
     setting :page_description, "Cryptocurrency Development Activitiy. commits count, pull requests count, and more"
-    setting :page_keywords, "cryptocurrency,coin,development,dev activitiy,github,commits count, pull requests count"
+    setting :page_keywords, "cryptocurrency,coin,development,bitcoin,btc,dev activitiy,github,commits count, pull requests count"
     setting :meta do
       setting :ogp do
         setting :type, "website"
