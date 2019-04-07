5d86c04655c526ab81bfc3fd2b256a77ec7e7d24
horizon67 <sir.yes.sir.maru@gmail.com>
2019-04-07 14:37:04 +0900

    fix sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,1 @@
 Sidekiq::Logging.logger.level = Logger::DEBUG
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:16379/12' }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localohost:16379/12' }
-end
