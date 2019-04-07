95a9266986d6b782c7b8875a7aa6e0975afbdc39
horizon67 <sir.yes.sir.maru@gmail.com>
2019-04-07 13:28:09 +0900

    fix ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :branch, 'master'
 set :deploy_to, "/home/app/cryptocoin"
 set :keep_releases, 5
 set :rbenv_type, :user
-set :rbenv_ruby, '2.4.2'
+set :rbenv_ruby, '2.6.2'
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_path, '/home/app/.rbenv'
 set :rbenv_roles, :all
